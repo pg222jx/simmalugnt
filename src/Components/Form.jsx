92ea8db6c11bebd2538efdcede7bf902dd216f4c
Pernilla Göth <pg222jx@student.lnu.se>
2021-02-13 17:53:42 +0100

    Rendering correct page depending on post response

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,27 +3,49 @@ import { SuccessContext } from './SuccessContext'
 
 const Form = () => {
     const [success, setSuccess] = useContext(SuccessContext) // eslint-disable-line no-unused-vars
+    const endPoint = 'https://form-test.simmalugnt.dev/wp-json/simmalugnt/v1/form'
 
-    const handleEvent = (event) => { 
+    const handleEvent =  async (event) => { 
         event.preventDefault()
         const email = document.querySelector('.email').value
-        const emailObject = JSON.stringify({ email: email})
-        if (email === 'hello') {
+        const response = await getResponse(email)
+   
+        if (response.ok) {
+            setSuccess(true)
+        } else {
             const error = document.querySelector('.error')
             error.style.display = 'inline'; 
-            console.log(emailObject)
-        } else {
-            setSuccess(true)
         }
     } 
 
+    const getResponse = email => {
+        return new Promise((resolve) => {
+            const data = {
+              email: email
+            }
+
+            try {
+              const response = fetch(endPoint, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              resolve(response)
+            } catch (error) {
+              console.error('Error:', error)
+            }
+          })
+    }
+
     return ( 
             <form className="form" onSubmit={ handleEvent }>
-            <input className="email" type="text" id="mail" name="mail" placeholder="Your email here"/><br />
-            <hr className="line"/>
-            <p className="error">Error, check your email.<br/></p>
-            <br/>
-            <input className="submit" type="submit" value="Submit"/>
+                <input className="email" type="text" id="mail" name="mail" placeholder="Your email here"/><br />
+                <hr className="line"/>
+                <p className="error">Error, check your email.<br/></p>
+                <br/>
+                <input className="submit" type="submit" value="Submit"/>
             </form> 
      )
 }
